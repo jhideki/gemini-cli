21154b49a088148f31098d037a989fca49049141
jonathan-ami <126116089+jonathan-ami@users.noreply.github.com>
2024-04-25 21:46:34 -0700

    add streaming stil need to fix json stream

--- src/genai.rs
+++ src/genai.rs
@@ -1,36 +1,42 @@
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct Content {
     role: String,
     parts: Vec<Part>,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct Part {
     text: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct RequestBody {
     contents: Vec<Content>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct SafetyRating {
     category: String,
     probability: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Candidate {
     content: Content,
-    #[serde(rename = "finishedReason")]
-    finished_reason: String,
+    #[serde(rename = "finishReason")]
+    finish_reason: String,
     index: u32,
-    #[serde(rename = "SafetyRatings")]
+    #[serde(rename = "safetyRatings")]
     safety_ratings: Vec<SafetyRating>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    candidates: Vec<Candidate>,
 }
 
 pub struct Genai {
@@ -38,6 +44,7 @@ pub struct Genai {
     model_name: String,
     end_point: String,
     message_thread: Vec<Content>,
+    client: Client,
 }
 impl Genai {
     pub fn new(api_key: String, model_name: &str) -> Self {
@@ -46,6 +53,7 @@ impl Genai {
             model_name: model_name.to_string(),
             end_point: "https://generativelanguage.googleapis.com/v1beta/models/".to_string(),
             message_thread: Vec::new(),
+            client: Client::new(),
         }
     }
 
@@ -63,6 +71,7 @@ impl Genai {
             self.end_point, self.model_name, self.api_key
         );
         println!("{}", url);
+        println!("{}", serde_json::to_string(&request_body).unwrap());
         let client = Client::new();
         let response = client
             .post(&url)
@@ -70,8 +79,9 @@ impl Genai {
             .json(&request_body)
             .send()
             .await?;
-        let json_response: Vec<Candidate> = response.json().await?;
-        println!("{}", json_response[0].content.parts[0].text);
+
+        let json_response: Value = response.json().await?;
+        println!("{}", json_response.to_string());
         Ok(())
     }
 
@@ -86,21 +96,30 @@ impl Genai {
             role: "user".to_string(),
             parts,
         });
+
         let url = format!(
-            "{}{}:generateContent?key={}",
+            "{}{}:streamGenerateContent?alt=sse&key={}",
             self.end_point, self.model_name, self.api_key
         );
+
         let request_body = RequestBody {
             contents: self.message_thread.clone(),
         };
-        let client = Client::new();
-        let response = client
+
+        let mut res = self
+            .client
             .post(&url)
             .header("Content-Type", "application/json")
             .json(&request_body)
             .send()
             .await?;
-        let json_response: Vec<Candidate> = response.json().await?;
+
+        while let Some(chunk) = res.chunk().await? {
+            let chunk = serde_json::Deserializer::from_slice(&chunk).into_iter::<Value>();
+            for val in chunk {
+                println!("{}", val.unwrap());
+            }
+        }
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,17 @@ use std::io::Write;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let api_key = env::var("GEMINI_API_KEY").expect("error loading env");
-    let client = Genai::new(api_key, "gemini-pro");
+    let mut client = Genai::new(api_key, "gemini-pro");
     loop {
         println!("Enter a prompt:");
         io::stdout().flush().unwrap();
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        let _ = io::stdin().read_line(&mut input);
         let trimmed = input.trim();
         if trimmed == "exit" {
             break;
         }
-        client.single_query(input).await?;
+        client.message_thread(input).await?;
     }
     println!("Ending session");
     Ok(())
